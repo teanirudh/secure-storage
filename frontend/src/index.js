f056cdc0d57afd8f5191a3b3af1a10ae8ce8fc87
teanirudh <teanirudh@gmail.com>
2023-03-05 22:11:42 +0530

    react-setup

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+        <h1>secure-storage</h1>
+    </div>
+  </React.StrictMode>
+);
