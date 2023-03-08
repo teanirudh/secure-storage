0be00e7775b1a2c4b7d223210555bc295e0df582
teanirudh <teanirudh@gmail.com>
2023-03-08 11:07:31 +0530

    formatters-setup

--- backend/manage.py
+++ backend/manage.py
@@ -6,9 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ssm.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ssm.settings")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import \
+            execute_from_command_line  # type: ignore
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -18,5 +19,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- backend/ssm/views.py
+++ backend/ssm/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 
+
 def index(request):
-   text = """<center><h1>secure-storage</h1></center>"""
-   return HttpResponse(text)
+    text = """<center><h1>secure-storage</h1></center>"""
+    return HttpResponse(text)
